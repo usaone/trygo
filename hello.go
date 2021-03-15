034cb7cc2c09e77901e89f6b2922be48bb0fb242
Uddip Mitra <uddipmitra@gmail.com>
2021-03-15 09:08:22 -0400

    Not sure what I changed in Oct 2020

--- hello.go
+++ hello.go
@@ -2,7 +2,13 @@ package main
 
 import "fmt"
 
+// This is an import block, it allows multiple packages to be imported without
+// repeating the "import" keyword.
+
+/*
+The main function, when part of the main package, identifies
+the entry point of an application.
+*/
 func main() {
 	fmt.Printf("hello, world\n")
 }
-
